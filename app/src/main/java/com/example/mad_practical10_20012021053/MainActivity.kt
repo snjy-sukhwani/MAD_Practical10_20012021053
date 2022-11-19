f836974a8728ee5c49fac572ad438446bcb6064f
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-11-19 12:22:24 +0530

    Update : Main Activity xml & kt file

--- app/src/main/java/com/example/mad_practical10_20012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical10_20012021053/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.mad_practical10_20012021053
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.mad_practical10_20012021053.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.fab.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    // from json-generator.com
+                    val data = HttpRequest().makeServiceCall(
+                        "https://api.json-generator.com/templates/V3pa8WdSsk-Z/data",
+                        "q05nu7pywmymigamhspxkt00iwjopybainqgmy59")
+                    withContext(Dispatchers.Main) {
+                        try {
+                            if(data != null)
+                                runOnUiThread{getPersonDetailsFromJson(data)}
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+    private fun getPersonDetailsFromJson(sJson: String?) {
+        val personList = ArrayList<Person>()
+        try {
+            val jsonArray = JSONArray(sJson)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray[i] as JSONObject
+                val person = Person(jsonObject)
+                personList.add(person)
+            }
+            binding.listView1.adapter = PersonAdapter(this, personList)
+        } catch (ee: JSONException) {
+            ee.printStackTrace()
+        }
     }
 }
